a28b4bcc818b2079c74f8d37c8efeaad813a70b2
Tharak <tharak@Tharaks-MacBook-Air.local>
2018-07-06 14:38:43 +0530

    angular cli add

--- src/app/service/config.service.ts
+++ src/app/service/config.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class ConfigService {
-  public _base_url = 'http://staging.bankopen.co/api/';
+  public _base_url = 'https://staging.bankopen.co/api/';
   public _intercom_id = 'fq4nevwt';
   public _mixpanel_id = '2ff23e024646457506a65b329c087772';
   constructor() {}
